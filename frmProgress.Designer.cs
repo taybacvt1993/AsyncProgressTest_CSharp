49afa504297bdfcee0cdbf91aa2a1de773b818d7
Nguyen Van Bac <taybacvt1993@gmail.com>
2020-11-24 15:36:12 +0700

    Init project

---
+++ frmProgress.Designer.cs
@@ -0,0 +1,155 @@
+﻿namespace BackgroundWorkerApp
+{
+    partial class frmProgress
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.bgwProcess = new System.ComponentModel.BackgroundWorker();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.progressBar = new System.Windows.Forms.ProgressBar();
+            this.lblPercent = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblPercentTask = new System.Windows.Forms.Label();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.SuspendLayout();
+            // 
+            // bgwProcess
+            // 
+            this.bgwProcess.WorkerReportsProgress = true;
+            this.bgwProcess.WorkerSupportsCancellation = true;
+            this.bgwProcess.DoWork += new System.ComponentModel.DoWorkEventHandler(this.bgwProcess_DoWork);
+            this.bgwProcess.ProgressChanged += new System.ComponentModel.ProgressChangedEventHandler(this.bgwProcess_ProgressChanged);
+            this.bgwProcess.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.bgwProcess_RunWorkerCompleted);
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(152, 99);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 23);
+            this.btnStart.TabIndex = 0;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // btnStop
+            // 
+            this.btnStop.Location = new System.Drawing.Point(233, 99);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(75, 23);
+            this.btnStop.TabIndex = 1;
+            this.btnStop.Text = "Stop";
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // progressBar
+            // 
+            this.progressBar.Location = new System.Drawing.Point(12, 24);
+            this.progressBar.Name = "progressBar";
+            this.progressBar.Size = new System.Drawing.Size(296, 23);
+            this.progressBar.TabIndex = 2;
+            // 
+            // lblPercent
+            // 
+            this.lblPercent.Location = new System.Drawing.Point(264, 8);
+            this.lblPercent.Name = "lblPercent";
+            this.lblPercent.Size = new System.Drawing.Size(44, 13);
+            this.lblPercent.TabIndex = 3;
+            this.lblPercent.Text = "0%";
+            this.lblPercent.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 8);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(103, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Background Worker";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 54);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(68, 13);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Task Parallel";
+            // 
+            // lblPercentTask
+            // 
+            this.lblPercentTask.Location = new System.Drawing.Point(264, 54);
+            this.lblPercentTask.Name = "lblPercentTask";
+            this.lblPercentTask.Size = new System.Drawing.Size(44, 13);
+            this.lblPercentTask.TabIndex = 8;
+            this.lblPercentTask.Text = "0%";
+            this.lblPercentTask.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(12, 70);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(296, 23);
+            this.progressBar1.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(320, 131);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lblPercentTask);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblPercent);
+            this.Controls.Add(this.progressBar);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.btnStart);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Processing";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.ComponentModel.BackgroundWorker bgwProcess;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.ProgressBar progressBar;
+        private System.Windows.Forms.Label lblPercent;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblPercentTask;
+        private System.Windows.Forms.ProgressBar progressBar1;
+    }
+}
+
---
+++ frmProgress.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BackgroundWorkerApp
+{
+    public partial class frmProgress : Form
+    {
+        public frmProgress()
+        {
+            InitializeComponent();
+        }
+
+        struct DataProcess {
+            public int Process;
+            public int Delay;
+        };
+
+        private async void btnStart_Click(object sender, EventArgs e)
+        {
+            DataProcess setting = new DataProcess()
+            {
+                Delay = 10,
+                Process = 100
+            };
+
+            if (!bgwProcess.IsBusy)
+            {
+                bgwProcess.RunWorkerAsync(setting);
+            }
+
+            var progress = new Progress<ProgressReport>();
+            progress.ProgressChanged += Task_ProgressChanged;
+
+            await ProcessData_Task(setting, progress);
+            MessageBox.Show("Task has been completed", "Message", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            if (bgwProcess.IsBusy)
+            {
+                bgwProcess.CancelAsync();
+            }
+        }
+
+        private void bgwProcess_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            progressBar.Value = e.ProgressPercentage;
+            lblPercent.Text = e.ProgressPercentage + " %";
+        }
+
+        private void bgwProcess_DoWork(object sender, DoWorkEventArgs e)
+        {
+            int process = ((DataProcess)e.Argument).Process;
+            int delay = ((DataProcess)e.Argument).Delay;
+
+            try
+            {
+                for (int i = 0; i < process; i++)
+                {
+                    if (bgwProcess.CancellationPending)
+                    {
+                        e.Cancel = true;
+                        return;
+                    }
+
+                    Thread.Sleep(delay);
+                    int percentProcess = (i + 1) * 100 / process;
+                    bgwProcess.ReportProgress(percentProcess);
+                    Console.WriteLine($"Processing: {percentProcess}%");
+                }
+            }
+            catch (Exception ex)
+            {
+                bgwProcess.CancelAsync();
+                MessageBox.Show(ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void bgwProcess_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            if (e.Cancelled)
+            {
+                Console.WriteLine("Process has been cancelled");
+            }
+            else
+            {
+                MessageBox.Show("Process has been completed", "Message", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+
+        private Task ProcessData_Task(DataProcess setting, IProgress<ProgressReport> progress)
+        {
+            int process = setting.Process;
+            int delay = setting.Delay;
+
+            return Task.Run(() =>
+            {
+                for (int i = 0; i < process; i++)
+                {
+                    Thread.Sleep(delay * 2);
+                    int percentProcess = (i + 1) * 100 / process;
+                    //UpdateProgressTask(percentProcess);
+                    progress.Report(new ProgressReport() { PercentComplete = percentProcess });
+
+                    Console.WriteLine($"Task: {percentProcess}%");
+                }
+            });
+        }
+
+        private void UpdateProgressTask(int percent)
+        {
+            this.Invoke((MethodInvoker)delegate
+            {
+                progressBar1.Value = percent;
+                lblPercentTask.Text = percent + " %";
+            });
+        }
+
+        private void Task_ProgressChanged(object sender, ProgressReport e)
+        {
+            progressBar1.Value = e.PercentComplete;
+            lblPercentTask.Text = e.PercentComplete + " %";
+        }
+
+        class ProgressReport
+        {
+            public int PercentComplete { get; set; }
+        }
+    }
+}
